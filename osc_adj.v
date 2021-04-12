f0de39e0474ba3f59826eca08788f9477985a0b8
Dan Stan <dstanley@Stanford.edu>
2021-04-12 11:43:20 -0700

    Create dummy verilog

---
+++ osc_adj.v
@@ -0,0 +1,11 @@
+// dummy verilog module for the adjustable oscillator
+// TODO How should power and ground be declared?
+
+module osc_adj (
+    input vdd,
+    input vss,
+    input ctrl0,
+    input ctrl1,
+    output out
+);
+endmodule
